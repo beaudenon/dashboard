0b51061a7020cddc298f61152ddd4651a99c3fca
root <root@195-154-10-49.online.net>
2016-01-10 15:18:20 +0100

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Wikidate::Application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  devise_for :users
+  #devise_for :users
 
   resources :secubat_clients do
     member do
@@ -85,6 +85,6 @@ Wikidate::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  #match ':controller(/:action(/:id(.:format)))'
   root :to => 'projects#index'
 end
